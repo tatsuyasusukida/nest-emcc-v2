6daa50c428edc1eb0c05489905065f019927f5a6
susukida <susukida@loremipsum.co.jp>
2023-07-21 17:37:09 +0900

    CORS オリジンをカンマ区切りで複数指定できるようにしました。

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,26 @@ import { AppModule } from './app.module';
 import { json } from 'express';
 
 async function bootstrap() {
+  const allowlist = (process.env.CORS_ORIGIN ?? '')
+    .split(',')
+    .map((origin) => origin.trim())
+    .filter((origin) => origin !== '');
+
   const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: process.env.CORS_ORIGIN,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
+    cors: (req, callback) => {
+      const origin = allowlist.includes(req.header('Origin'));
+
+      callback(null as any, {
+        origin,
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+      });
     },
   });
 
   app.use(json({ limit: '10mb' }));
-  await app.listen(process.env.PORT || '3000');
+  await app.listen(process.env.PORT ?? '3000');
 }
+
 bootstrap();
